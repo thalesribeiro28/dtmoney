aa087546e07000e6f699c448a8c1b6ec53bf48f0
= <=>
2022-05-24 21:46:22 -0300

    Final da fase 02

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+
+  models: { 
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions : [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          amount: 6000,
+          category: 'Dev',
+          createdAt: new Date('2021-02-12 08:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          amount: 3200,
+          category: 'Casa',
+          createdAt: new Date('2022-05-12 15:00:00'),
+        }
+      ],
+    })
+  },
+
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transactions/', () => {
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions/', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +55,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
